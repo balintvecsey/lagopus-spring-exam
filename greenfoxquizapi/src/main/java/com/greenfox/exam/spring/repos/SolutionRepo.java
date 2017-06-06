928f68679f5d8c396c8504c4f5a9bf6964aa27dd
balintvecsey <balint.vecsey@gmail.com>
2017-06-06 10:19:23 +0200

    add SolutionRepo interface

---
+++ greenfoxquizapi/src/main/java/com/greenfox/exam/spring/repos/SolutionRepo.java
@@ -0,0 +1,10 @@
+package com.greenfox.exam.spring.repos;
+
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by Bálint on 2017. 06. 06..
+ */
+public interface SolutionRepo extends CrudRepository {
+
+}
